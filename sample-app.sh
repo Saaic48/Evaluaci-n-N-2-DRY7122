ce7388cf1f98d819189effc12437d7955dca0cb3
NetAcad DEVASC <changeme@example.com>
2026-05-29 17:50:14 +0000

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker stop samplerunning 2>/dev/null
+docker rm samplerunning 2>/dev/null
+rm -rf tempdir
+
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -9,7 +13,7 @@ cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
